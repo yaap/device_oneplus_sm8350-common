abbcfabfdc3151776b659d3d8a0861344ed48167
John Galt <johngaltfirstrun@gmail.com>
2026-05-26 10:12:54 -0400

    Revert "sm8350-common: Remove fwk-detect lib from PRODUCT_PACKAGES"
    This reverts commit 5401742945aea3701dbb31103c47b683b9b20486.

--- common.mk
+++ common.mk
@@ -252,6 +252,10 @@ PRODUCT_COPY_FILES += \
 
 $(call soong_config_set,qtipower,tap_to_wake_node,/proc/touchpanel/double_tap_enable)
 
+# QTI fwk-detect
+PRODUCT_PACKAGES += \
+    libvndfwk_detect_jni.qti.vendor # Needed by CNE app
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.multihal \
